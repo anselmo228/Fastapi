24c51a8c142827a9737b2e7da16dfc13f02b7a80
anselmo228 <anselmo228@naver.com>
2023-09-20 13:50:49 +0900

    run 수정

--- run.py
+++ run.py
@@ -8,12 +8,15 @@ app.config = {"max_request_body_size": None}
 
 url = "http://13.125.245.102/upload/"
 
+### 1. main.py Upload 실행 ###
 files = {
     "file": ("/Users/jungheechan/AWS_Flask/output3_5.wav", open("/Users/jungheechan/AWS_Flask/output3_5.wav", "rb")),
     "access_key": (None, "ec2_test"),
 }
-
 response = requests.post(url, files=files)
-
 print(response.text)
+# response text = File uploaded successfully" 이면 성공
+# else 실패: upload fail or S3 접근 실패
 
+### 2. Spleeter.py : MR과 AR 분리 후 Upload 실행 ###
+### 3. Train.py: RVC 모델에서 학습시키기 실행 ###
